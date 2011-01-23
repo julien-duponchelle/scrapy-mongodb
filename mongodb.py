11e8f44f2976423e681a16e950329afb53a29546
Julien Duponchelle <julien@duponchelle.info>
2011-01-23 19:10:40 +0100

    Initial version

---
+++ mongodb.py
@@ -0,0 +1,46 @@
+# Copyright 2011 Julien Duponchelle <julien@duponchelle.info>.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""MongoDB Pipeline for scrappy"""
+
+import pymongo
+from scrapy.conf import settings
+from scrapy import log
+
+class MongoDBPipeline(object):
+    def __init__(self):
+        connection = pymongo.Connection(settings['MONGODB_SERVER'], settings['MONGODB_PORT'])
+        self.db = connection[settings['MONGODB_DB']]
+        self.collection = self.db[settings['MONGODB_COLLECTION']]
+        if self.__get_uniq_key() is not None:
+            self.collection.create_index(self.__get_uniq_key(), unique=True)
+
+    def process_item(self, item, spider):
+        if self.__get_uniq_key() is None:
+            self.collection.insert(dict(item))
+        else:
+            self.collection.update(
+                            {self.__get_uniq_key(): item[self.__get_uniq_key()]},
+                            dict(item),
+                            upsert=True)  
+        log.msg("Item wrote to MongoDB database %s/%s" %
+                    (settings['MONGODB_DB'], settings['MONGODB_COLLECTION']),
+                    level=log.DEBUG, spider=spider)  
+        return item
+
+    def __get_uniq_key(self):
+        if not settings['MONGODB_UNIQ_KEY'] or settings['MONGODB_UNIQ_KEY'] == "":
+            return None
+        return settings['MONGODB_UNIQ_KEY']
+   
