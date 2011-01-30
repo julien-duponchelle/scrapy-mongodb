b93742f363479ecb3b5c4a52e2410a0d1f95dd6b
Julien Duponchelle <duponc_j@epitech.net>
2011-01-30 18:49:03 +0100

    Work on package building

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='Scrapy MongoDB',
+      version='0.1',
+      description='Scrapy pipeline which allow you to store scrapy items in MongoDB database.',
+      author='Julien Duponchelle',
+      author_email='julien@duponchelle.info',
+      url='http://github.com/noplay/scrapy-mongodb',
+      py_modules=['scrapy-mongodb'],
+      )
