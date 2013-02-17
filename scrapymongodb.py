6ad3634e670f6e2e82f7260b66713ed84df0eed4
Jordi Llonch <llonchj@gmail.com>
2013-02-18 07:40:24 +1100

    fixed typo

--- scrapymongodb.py
+++ scrapymongodb.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""MongoDB Pipeline for scrappy"""
+"""MongoDB Pipeline for scrapy"""
 
 import pymongo
 from scrapy.conf import settings
