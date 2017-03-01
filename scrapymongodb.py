4fb170f5599347ec83d652abffb9f394e6fdad80
Valeriy Osipov <info@vosipov.com>
2017-03-01 18:10:02 +0300

    Fix basestring compatibility issue with Python 3.X and some PEP-8 formatting

--- scrapymongodb.py
+++ scrapymongodb.py
@@ -16,10 +16,12 @@
 
 import logging
 import pymongo
+import six
 
 MONGODB_ITEM_ID_FIELD = "_id"
 
 logger = logging.getLogger(__name__)
+
 
 class MongoDBPipeline(object):
     def __init__(self, mongodb_server, mongodb_port, mongodb_db, mongodb_collection, mongodb_uniq_key,
@@ -30,14 +32,13 @@ class MongoDBPipeline(object):
         self.mongodb_collection = mongodb_collection
         self.collection = self.db[mongodb_collection]
         self.uniq_key = mongodb_uniq_key
-        self.itemid = mongodb_item_id_field
+        self.item_id = mongodb_item_id_field
 
-        if isinstance(self.uniq_key, basestring) and self.uniq_key == "":
+        if isinstance(self.uniq_key, six.string_types) and self.uniq_key == "":
             self.uniq_key = None
-            
+
         if self.uniq_key:
             self.collection.ensure_index(self.uniq_key, unique=True)
-
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -46,17 +47,17 @@ class MongoDBPipeline(object):
                    settings.get('MONGODB_DB', 'scrapy'), settings.get('MONGODB_COLLECTION', None),
                    settings.get('MONGODB_UNIQ_KEY', None), settings.get('MONGODB_ITEM_ID_FIELD', MONGODB_ITEM_ID_FIELD))
 
-
     def process_item(self, item, spider):
         if self.uniq_key is None:
             result = self.collection.insert_one(dict(item))
         else:
-            result = self.collection.update_one({ self.uniq_key: item[self.uniq_key] }, { '$set': dict(item) },
-                                            upsert=True)
+            result = self.collection.update_one({self.uniq_key: item[self.uniq_key]}, {'$set': dict(item)},
+                                                upsert=True)
 
         # If item has _id field and is None
-        if self.itemid in item.fields and not item.get(self.itemid, None):
-            item[self.itemid] = result
+        if self.item_id in item.fields and not item.get(self.item_id, None):
+            item[self.item_id] = result
 
-        logger.debug("Item %s wrote to MongoDB database %s/%s, spider: %s" % (result.inserted_id, self.mongodb_db, self.mongodb_collection, spider.name))
+        logger.debug("Item %s wrote to MongoDB database %s/%s, spider: %s" % (
+            result.inserted_id, self.mongodb_db, self.mongodb_collection, spider.name))
         return item
